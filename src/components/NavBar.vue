1a53f3119c1c36fc2af14cb46225f87695296104
Omar Minaya <omiinaya@gmail.com>
2022-10-27 16:28:45 -0400

    spacing

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,39 +1,9 @@
 <template>
-  <!-- Navbar 
-  <nav class="navbar">
-    <div class="container-fluid">
-      <form class="d-flex" role="search">
-        <a class="navbar-brand" href="#">Mobile Me IT Inc.</a>
-        <input class="form-control me-2" type="search" placeholder="Search all the things" aria-label="Search">
-      </form>
-    </div>
-  </nav>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container container-fluid">
-      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars text-light"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto d-flex flex-row mt-3 mt-lg-0">
-          <li class="nav-item text-center mx-2 mx-lg-1" v-for="(link, index) in links" :key="link + index">
-            <a class="nav-link active" aria-current="page" href="#!">
-              <div>
-                <i :class=link.icon></i>
-              </div>
-              {{ link.title }}
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  -->
   <div class="row">
-    <div class="col-1 offset-1">
-        <a class="navbar-brand" href="#">Mobile Me IT Inc.</a>
+    <div class="col-1-0 offset-1 topBar">
+      <div>Mobile Me IT Inc.</div>
     </div>
-    <div class="col-8">
+    <div class="col-8 search">
       <input class="form-control me-2" type="search" placeholder="Search all the things" aria-label="Search" />
     </div>
   </div>
@@ -41,7 +11,7 @@
     <div class="col-1">
 
     </div>
-    <div class="col-1-5 nav-item text-center" v-for="(link, index) in links" :key="link + index">
+    <div class="col-0-5 nav-item text-center" v-for="(link, index) in links" :key="link + index">
       <a class="nav-link active" aria-current="page" href="#!">
         <div>
           <i :class=link.icon></i>
@@ -76,34 +46,33 @@ export default {
 </script>
 
 <style>
+.search {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  padding-top: 1vh;
+}
 
 .topBar {
-  text-align: center;
-}
-.container {
-  margin-left: 50px;
-}
-
-.navbar {
-  width: 100%;
+  padding-top: 2vh;
 }
 
 .bi {
   font-size: 45px;
 }
 
-.navbar-brand {
-  margin: auto;
-  color: white;
-}
-
 .form-control {
-  margin-left: 50px;
   width: 30% !important;
 }
 
-.col-1-5 {
+.col-0-5 {
   flex: 0 0 6.15%;
+  max-width: 6.15%;
+  position: relative;
+  width: 100%;
+}
+
+.col-1-0 {
+  flex: 0 0 12.30%;
   max-width: 6.15%;
   position: relative;
   width: 100%;
