260b479894984eea16250b3b12bd475139756042
Omar Minaya <omiinaya@gmail.com>
2022-10-27 21:59:41 -0400

    oof

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+import VeeValidate from 'vee-validate';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -29,7 +30,8 @@ import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(VeeValidate);
   
 router.isReady().then(() => {
   app.mount('#app');
