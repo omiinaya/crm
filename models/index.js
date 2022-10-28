1413685db86dc02b8974fdbd6a6cdc21ef458d10
Omar Minaya <omiinaya@gmail.com>
2022-10-28 13:06:42 -0400

    init in the right place.

--- models/index.js
+++ models/index.js
@@ -1,22 +1,15 @@
-
-const MYSQL = require('mysql2')
-const CONFIG = {
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD
-}
-
-const connection = MYSQL.createConnection(CONFIG);
-connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB}\`;`);
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
     operatorsAliases: 0,
-    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
-});
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+  }
+);
 
 const db = {};
 
@@ -28,15 +21,15 @@ db.role = require("./role.model.js")(sequelize, Sequelize);
 db.navItem = require("./navItem.model.js")(sequelize, Sequelize);
 
 db.user.belongsToMany(db.role, {
-    through: "user_roles",
-    foreignKey: "userId",
-    otherKey: "roleId"
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
 });
 
 db.role.belongsToMany(db.user, {
-    through: "user_roles",
-    foreignKey: "roleId",
-    otherKey: "userId"
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
 });
 
 db.ROLES = ["user", "admin", "moderator"];
--- setup.js
+++ setup.js
@@ -1,73 +1,83 @@
 function setup(Role, NavItem) {
-    Role.create({
-        id: 1,
-        name: "user"
-    });
+  const MYSQL = require("mysql2");
+  const CONFIG = {
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+  };
 
-    Role.create({
-        id: 2,
-        name: "moderator"
-    });
+  const connection = MYSQL.createConnection(CONFIG);
+  connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB}\`;`);
 
-    Role.create({
-        id: 3,
-        name: "admin"
-    });
+  Role.create({
+    id: 1,
+    name: "user",
+  });
 
-    NavItem.create({
-        title: 'Customers',
-        icon: 'bi bi-person',
-        url: '#!'
-    })
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
 
-    NavItem.create({
-        title: 'Assets',
-        icon: 'bi bi-laptop',
-        url: '#!'
-    })
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
 
-    NavItem.create({
-        title: 'Invoices',
-        icon: 'bi bi-cart',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Customers",
+    icon: "bi bi-person",
+    url: "#!",
+  });
 
-    NavItem.create({
-        title: 'Estimates',
-        icon: 'bi bi-file-earmark-text',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Assets",
+    icon: "bi bi-laptop",
+    url: "#!",
+  });
 
-    NavItem.create({
-        title: 'Tickets',
-        icon: 'bi bi-tag',
-        url: '#!'
-    })
-    
-    NavItem.create({
-        title: 'Parts',
-        icon: 'bi bi-motherboard',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Invoices",
+    icon: "bi bi-cart",
+    url: "#!",
+  });
 
-    NavItem.create({
-        title: 'Inventory',
-        icon: 'bi bi-upc-scan',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Estimates",
+    icon: "bi bi-file-earmark-text",
+    url: "#!",
+  });
 
-    NavItem.create({
-        title: 'POS',
-        icon: 'bi bi-cash',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Tickets",
+    icon: "bi bi-tag",
+    url: "#!",
+  });
 
-    NavItem.create({
-        title: 'Admin',
-        icon: 'bi bi-gear',
-        url: '#!'
-    })
+  NavItem.create({
+    title: "Parts",
+    icon: "bi bi-motherboard",
+    url: "#!",
+  });
 
+  NavItem.create({
+    title: "Inventory",
+    icon: "bi bi-upc-scan",
+    url: "#!",
+  });
+
+  NavItem.create({
+    title: "POS",
+    icon: "bi bi-cash",
+    url: "#!",
+  });
+
+  NavItem.create({
+    title: "Admin",
+    icon: "bi bi-gear",
+    url: "#!",
+  });
 }
 
 module.exports = setup;
