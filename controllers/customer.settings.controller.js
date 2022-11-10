7a338d434345367ddf1e86acb098c12600a31799
Omar Minaya <omiinaya@gmail.com>
2022-11-10 15:57:52 -0500

    WORKS.

---
+++ controllers/customer.settings.controller.js
@@ -0,0 +1,126 @@
+const db = require("../models");
+const CustomerSettings = db.customerSettings;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+
+  const customerSettings = {
+    title: req.body.title,
+    icon: req.body.description,
+    url: req.body.published,
+  };
+
+  CustomerSettings.create(customerSettings)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Nav.",
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  CustomerSettings.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+exports.findByRoleId = (req, res) => {
+  const id = req.params.id;
+
+  CustomerSettings.findAll({
+    where: { roleId: id },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+exports.findByRole = (req, res) => {
+  const id = req.params.id;
+  CustomerSettings.findAll({
+    where: {
+      roleId: {
+        [Op.lte]: id,
+      },
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  CustomerSettings.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id + err,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  CustomerSettings.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id + err,
+      });
+    });
+};
