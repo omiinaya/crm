b3ce4ecd84f752df9480ad106f633c9a5ba8d6de
Omar Minaya <omiinaya@gmail.com>
2022-11-15 16:03:43 -0500

    icons for asset creation form.

--- setup/asset.fields.data.js
+++ setup/asset.fields.data.js
@@ -4,7 +4,7 @@ const assetFields = [
         label: "Asset Type",
         type: "text",
         placeholder: "Asset Type",
-        icon: "bi bi-person",
+        icon: "bi bi-list-check",
         side: 0,
     },
     {
@@ -20,7 +20,7 @@ const assetFields = [
         label: "Asset Name",
         type: "text",
         placeholder: "Asset Name",
-        icon: "bi bi-person",
+        icon: "bi bi-laptop",
         side: 0,
     },
     {
@@ -28,7 +28,7 @@ const assetFields = [
         label: "Asset Serial",
         type: "text",
         placeholder: "Asset Serial",
-        icon: "bi bi-building",
+        icon: "bi bi-laptop",
         side: 0,
     },
     {
@@ -36,7 +36,7 @@ const assetFields = [
         label: "Manufacturer",
         type: "text",
         placeholder: "Manufacturer",
-        icon: "bi bi-mortarboard",
+        icon: "bi bi-nut",
         side: 0,
     },
     {
@@ -44,7 +44,7 @@ const assetFields = [
         label: "Asset Tag",
         type: "text",
         placeholder: "Asset Tag",
-        icon: "bi bi-envelope",
+        icon: "bi bi-tag",
         side: 0,
     }
 ];
