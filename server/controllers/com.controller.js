522a721c3d60f33be1e699d3d575634dde486134
Omar Minaya <omiinaya@gmail.com>
2022-12-15 17:16:26 -0500

    cleaning up code.

--- server/controllers/com.controller.js
+++ server/controllers/com.controller.js
@@ -53,17 +53,9 @@ exports.create = async (req, res) => {
   const comFields = await axios.get(
     "http://localhost:8090/api/com/fields"
   );
-  const comResponse = await comFields.data;
 
-  comResponse[comResponse.length] = {
-    id: 7,
-    name: 'customerPhone'
-  }
-
-  comResponse[comResponse.length] = {
-    id: 8,
-    name: 'customerEmail'
-  }
+  let comResponse = await comFields.data;
+  comResponse = [...comResponse, { id: 7, name: "customerPhone" }, { id: 8, name: "customerEmail" }];
 
   let com = {};
 
@@ -84,13 +76,18 @@ exports.create = async (req, res) => {
 
     socket.emit("comCreatedRequest", ticketId);
 
-    if (visibility === 'Email + SMS') {
-      sendText(message, twilio, number)
-      sendMail(email, subject, message)
-    } else if (visibility === 'SMS') {
-      sendText(message, twilio, number)
-    } else if (visibility === 'Email') {
-      sendMail(email, subject, message)
+    // Use switch statement instead of if/else
+    switch (visibility) {
+      case "Email + SMS":
+        sendText(message, twilio, number);
+        sendMail(email, subject, message);
+        break;
+      case "SMS":
+        sendText(message, twilio, number);
+        break;
+      case "Email":
+        sendMail(email, subject, message);
+        break;
     }
 
     res.send(await request);
@@ -99,97 +96,91 @@ exports.create = async (req, res) => {
   }
 };
 
-exports.findAll = (req, res) => {
-  Com.findAll()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
+exports.findAll = async (req, res) => {
+  try {
+    const data = await Com.findAll();
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving tutorials.",
     });
+  }
 };
 
-exports.findByTicketId = (req, res) => {
-  const id = req.params.id;
-
-  Com.findAll({
-    where: { comTicketId: id },
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
+exports.findByTicketId = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Com.findAll({
+      where: { comTicketId: id },
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving tutorials.",
+    });
+  }
 };
 
-exports.findByAuthorId = (req, res) => {
+exports.findByAuthorId = async (req, res) => {
   const id = req.params.id;
 
-  Com.findAll({
-    where: { comAuthorId: id },
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
+  try {
+    const data = await Com.findAll({
+      where: { comAuthorId: id },
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while retrieving tutorials.",
+    });
+  }
 };
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
 
-  Com.update(req.body, {
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: "Tutorial was updated successfully.",
-        });
-      } else {
-        res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: "Error updating Tutorial with id=" + id + err,
-      });
+  try {
+    const num = await Com.update(req.body, {
+      where: { id: id },
     });
+
+    if (num == 1) {
+      res.send({
+        message: "Tutorial was updated successfully.",
+      });
+    } else {
+      res.send({
+        message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Error updating Tutorial with id=" + id + err,
+    });
+  }
 };
 
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
 
-  Ticket.destroy({
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: "Tutorial was deleted successfully!",
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id + err,
-      });
+  try {
+    const num = await Ticket.destroy({
+      where: { id: id },
     });
+
+    if (num == 1) {
+      res.send({
+        message: "Tutorial was deleted successfully!",
+      });
+    } else {
+      res.send({
+        message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete Tutorial with id=" + id + err,
+    });
+  }
 };
